7b2c3d2821a489a2ceb0235765e380f6f6b799b1
sk89q <the.sk89q@gmail.com>
2009-04-13 17:10:55 -0700

    initial commit

---
+++ README.txt
@@ -0,0 +1,14 @@
+WavForge
+Copyright (c) sk89q <http://sk89q.therisenrealm.com>
+
+Requirements
+------------
+
+* PHP 5.0 or later
+
+Introduction
+------------
+
+WavForge is a proof-of-concept class that generates some waveforms and
+outputs PCM WAV files. It's also a bit slow, but this is only a proof
+of concept demo.
---
+++ examples/tone.php
@@ -0,0 +1,20 @@
+<?php // $Id$
+// WavForge example
+
+require_once "../WavForge.php";
+
+$wf = new WavForge();
+$wf->setBitsPerSample(16);
+$wf->synthesizeSine(440, 1, 1); // 440 Hz, 100% volume, 1 second
+$wf->synthesizeSawtooth(440, 1, 1); // 400 Hz, 100% volume, 1 second
+$wf->synthesizeNoise(1, 1); // 100% volume, 1 second
+
+if (php_sapi_name() == 'cli') { // Command line?
+    $fp = fopen("tone_test.wav", "wb");
+    fwrite($fp, $wf->getWAVData());
+    fclose($fp);
+    echo "tone_test.wav written\n";
+} else {
+    header("Content-Type: audio/wav");
+    echo $wf->getWAVData();
+}
